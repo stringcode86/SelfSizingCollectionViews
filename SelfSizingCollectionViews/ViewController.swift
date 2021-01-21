57b53a780144be20a30fef15d0994144c3a0066a
stringcode <michael@stringcode.co.uk>
2021-01-21 16:45:38 +0000

    Initial Commit

---
+++ SelfSizingCollectionViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfSizingCollectionViews
+//
+//  Created by stringcode on 21/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
